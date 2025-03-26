784aae8b7faa6fc0da8841a208ff8b4aab8345d2
Leonardo Marinho Pinto <leomarinho1000@outlook.com>
2025-03-26 11:54:59 -0300

    Initial commit

---
+++ src/main/java/me/dio/criando_uma_api_restful/CriandoUmaApiRestfulApplication.java
@@ -0,0 +1,13 @@
+package me.dio.criando_uma_api_restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriandoUmaApiRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriandoUmaApiRestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/criando_uma_api_restful/CriandoUmaApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.criando_uma_api_restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriandoUmaApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
